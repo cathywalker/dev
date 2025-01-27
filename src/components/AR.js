f1264295ac1f7ca5f8f98d321f160f9341ab3ab1
cathywalker <48182560+cathywalker@users.noreply.github.com>
2025-01-27 19:55:04 +0000

    AR

--- src/components/AR.js
+++ src/components/AR.js
@@ -4,7 +4,7 @@ const AR = () => {
   return (
     <div>
       <h1>Augmented Reality and Spatial Computing</h1>
-      <div>I am currently developing two prototypes for the Vision Pro, using Xcode, Reality Composer Pro, Swift, and SwiftUI. One of which I plan to deploy to the Apple Store early next year.</div>
+      <div>I am currently developing two prototypes for the Vision Pro, using Xcode, Reality Composer Pro, Swift, and SwiftUI. One of which I plan to deploy to the Apple Store this year.</div>
       <div><p>I previously made an AR App for Botanic Gardens in Belfast. It recognises various landmarks at the park such as the statue of Kelvin, Sculptures and the Bandstands and displays information about them beside the landmark and some of them also trigger audio.</p></div>
       <img
           src="https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-700/botanic_LiZ8mGTkU.jpg"
