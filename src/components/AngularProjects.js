3263c15d55b8896527b66e69652a82a6e8043886
cathywalker <48182560+cathywalker@users.noreply.github.com>
2025-01-27 19:46:14 +0000

    Projects

--- src/components/AngularProjects.js
+++ src/components/AngularProjects.js
@@ -4,7 +4,7 @@ const AngularProjects = () => {
   return (
     <div>
       <h1>Angular Projects</h1>
-      <div>I began developing in Angular in 2018. Some of the work I got from Clients was requested in this Framework. It is a steep learning curve but a fully fledged Framework once you can get over the hump. I am creating an Angular to React translation course and React to Angular Translation Course which will be available in Late 2023. The past couple of years I have developed new features for and maintained a large HR website in Angular. I upgraded it to Angular 16 so we could utilise Signals. This year I also developed, using Angular, a new Insurance Quote Engine, which has a projected uptake in revenue for the client of £6 million. <p>Below is a taster of my some of my other Angular projects with links to GitHub:</p> </div>
+      <div>I began developing in Angular in 2018. Some of the work I got from Clients was requested in this Framework. It is a steep learning curve but a fully fledged Framework once you can get over the hump. I am creating an Angular to React translation course and React to Angular Translation Course. The past couple of years I have developed new features for and maintained a large HR website in Angular. I upgraded it to Angular 16 so we could utilise Signals. This year I also developed, using Angular, a new Insurance Quote Engine, which has a projected uptake in revenue for the client of £6 million. <p>Below is a taster of my some of my other Angular projects with links to GitHub:</p> </div>
       <div>
         <h2>Cat Facts</h2>    
         <p>A website to display Cat Facts, sign up for a Cat Facts Newsletter and play a 'Raining Cats' Mini Game.</p>     
