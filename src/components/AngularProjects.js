db4ff4f2cc503c56dd3e8c3d4d3c1f4b17a39fcd
cathywalker <48182560+cathywalker@users.noreply.github.com>
2025-02-17 15:53:51 +0000

    AngularProjects

--- src/components/AngularProjects.js
+++ src/components/AngularProjects.js
@@ -4,7 +4,38 @@ const AngularProjects = () => {
   return (
     <div>
       <h1>Angular Projects</h1>
-      <div>I began developing in Angular in 2018. Some of the work I got from Clients was requested in this Framework. It is a steep learning curve but a fully fledged Framework once you can get over the hump. I am creating an Angular to React translation course and React to Angular Translation Course. The past couple of years I have developed new features for and maintained a large HR website in Angular. I upgraded it to Angular 16 so we could utilise Signals. In the past year I also developed, using Angular, a new Insurance Quote Engine, which has a projected uptake in revenue for the client of £6 million. <p>Below is a taster of my some of my other Angular projects with links to GitHub:</p> </div>
+      <div>I began developing in Angular in 2018. Some of the work I got from Clients was requested in this Framework. It is a steep learning curve but a fully fledged Framework once you can get over the hump. I am creating an Angular to React translation course and React to Angular Translation Course. The past couple of years I have developed new features for and maintained a large HR website in Angular. I upgraded it to Angular 16 so we could utilise Signals. In the past year I also developed, using Angular, a new Insurance Quote Engine, which has a projected uptake in revenue for the client of £6 million. <p>Below are links to some of my professional Angular projects and links to GitHub for any personal ones:</p> </div>
+      <div>
+        <h2>Talos</h2>    
+        <p>An applicant tracking system. Front-end made in Angular, TypeScript, SCSS, and Tailwind. Back-end in .NET and C#.</p> 
+        <p>I worked on 4 new Front-end UI features, included a new Onboarding portal.</p>    
+        <img
+          src="https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-700/TalosATS_bWWjbtd7O.png"
+          alt="ATS"
+          srcset="
+            https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-330/TalosATS_bWWjbtd7O.png 330w,
+            https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-700/TalosATS_bWWjbtd7O.png 700w,
+            https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-700/TalosATS_bWWjbtd7O.png 950w
+          "
+          sizes="(max-width: 950px) 330px, 700px"
+        />
+        <div><a href="https://talos360.co.uk/ats/" target="_blank">Talos ATS</a></div>
+      </div>
+      <div>
+        <h2>AF Quote Engine</h2>    
+        <p>Price Quote Engine for Pet Insurance. Front-end made in Angular, TypeScript, and SCSS</p>  
+        <img
+          src="https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-400/AFQuote_khuYVPtGe.png"
+          alt="Quote Engine"
+          srcset="
+            https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-330/AFQuote_khuYVPtGe.png 330w,
+            https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-400/AFQuote_khuYVPtGe.png 700w,
+            https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-400/AFQuote_khuYVPtGe.png 950w
+          "
+          sizes="(max-width: 950px) 330px, 700px"
+        />
+        <div><a href="https://www.animalfriends.co.uk/pet-insurance-quote-lp" target="_blank">AF - Price Quote Engine</a></div>
+      </div>
       <div>
         <h2>Cat Facts</h2>    
         <p>A website to display Cat Facts, sign up for a Cat Facts Newsletter and play a 'Raining Cats' Mini Game.</p>     
