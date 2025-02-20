fdf4d039a28a8878e6c8f12313e15d102c8b9268
cathywalker <48182560+cathywalker@users.noreply.github.com>
2025-02-20 17:43:19 +0000

    remove spaces

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,11 +9,7 @@ const AboutMe = () => {
 
         <p>I knew my future lay in Web Development as I loved learning about and implementing the techonology so much, and it had such a rich UI in comparison to other computing areas at the time. Since then I have kept up to date with latest Libraries, Frameworks and changes in languages. My main technologies at the minute are React, Angular, JavaScript, TypeScript, CSS, SCSS, and Tailwind.</p>
         <img src="https://ik.imagekit.io/dgmfafxequ/Dev/tr:w-300/me_2B1glwuHx.jpg" alt='Person in AR'/>
-
-     
       </div>
-    
-
     </div>
   );
 };
