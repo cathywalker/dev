323c1eb0fb5c095b05fbf3e9ffa815ddce5c8d5b
cathywalker <48182560+cathywalker@users.noreply.github.com>
2025-02-21 14:45:45 +0000

    text change

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -35,23 +35,25 @@ const Courses = () => {
           <h1>Taster of React to Angular conversion</h1>
           <div>
             <h2>For loops (.map = *ngFor)</h2>
-            <p>One of the syntaxes I find the most headachey in React is to map over objects and display some HTML (JSX) ultimately based on the changing variables. It don't look pretty and every time I find myself counting brackets. </p>
+            <p>One of the syntaxes I find the most headachey in React is to map over objects and display some HTML (JSX) ultimately based on the changing variables. It don't look pretty and every time I find myself counting brackets:</p>
             <h3>React Code</h3>
               <div style={styles.imageContainer}>
                 <ImageComponent images={reactMapJSXImage} altText={'React Map JSX code'}></ImageComponent>
                 <ImageComponent images={duckGif} altText={'Confused Duck by Jeremy Fisher'}></ImageComponent>            
               </div>
-              <p>Angular makes this a piece of cake with *ngFor. You simply add *ngFor in the HTML file <i>(or template as some like to call it)</i> and everything inside will repeat with the changing variable values of your favourite iterable data structure. What's even simpler is with an async pipe aka ' | async ' you can get the data from an Observable in the TypeScript file <i>(and it's always TS in angular, makes life easier. It helps you find those bugs at your desk at compile time rather than them shaking loose by a user after a deployment and a panicked roll back or hot fix)</i></p><p>You don't even have to write the clean up unsubscribe from the observable to stop those pesky memory leaks. It automatically does that for you with async. easy to read and memory leak proof. this is one of the syntaxes I really think there's no doubt is superior in Angular.</p>
-          
               <h3>Angular Code</h3>
+
+              <p>Angular makes this a piece of cake with *ngFor. You simply add *ngFor in the HTML file <i>(or template as some like to call it)</i> and everything inside will repeat with the changing variable values of your favourite iterable data structure.</p>
               <ImageComponent images={angularNgForImage} altText={'Angular ngFor code'}></ImageComponent>
+              
+              <p>What's even simpler is with an async pipe aka ' | async ' you can get the data from an Observable in the TypeScript file <i>(and it's always TS in angular, makes life easier. It helps you find those bugs at your desk at compile time rather than them shaking loose by a user after a deployment and a panicked roll back or hot fix)</i></p><p>You don't even have to write the clean up unsubscribe from the observable to stop those pesky memory leaks. It automatically does that for you with async. easy to read and memory leak proof. this is one of the syntaxes I really think there's no doubt is superior in Angular.</p>
           </div>
 
           <div>
           <h2 style={styles.header}>Content Projection (props.children = 'ng-content') </h2>
           <p><i>aka the info between your component brackets getting to the component</i></p>
 
-            <p>This one I'd give a tie to. Easy to learn the transition as, well, it's pretty much the same, bar different reserved words in the frameworks. Instead of <b>'props.children'</b> in react it's <b>'ng-content'</b> and then passing the children or projected ng-content as we'll now refer to it over in angular land between the tags of your parent component, wrapping just like you would child DOM nodes of a DIV since forever you have. Looks like someone copied their neighbours homework</p>
+            <p>This one I'd give a tie to. Easy to learn the transition as, well, it's pretty much the same, bar different reserved words in the frameworks. Instead of <b>'props.children'</b> in react it's <b>'ng-content'</b> and then passing the children or projected ng-content as we'll now refer to it over in angular land between the tags of your parent component, wrapping just like you would child DOM nodes of a DIV since forever you have. Looks like someone copied their neighbours homework:</p>
             <h3>React Code</h3>
               <div style={styles.imageContainer}>
                 <ImageComponent images={reactContentProjectionImage} altText={'React Map JSX code'}></ImageComponent>
